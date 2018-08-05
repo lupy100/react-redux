aab02336a1895e32376e4517fd9c3739d335b266
Joao Matheus <joao.matheus2819@gmail.com>
2018-08-05 20:53:26 -0300

    redux logger e applyMiddleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
+import logger from 'redux-logger'
 
 import reducer from './reducers'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// Manual 
+// const logger = store => next => action => {
+//   console.group(action.type)
+//   console.info('dispatching', action)
+//   let result = next(action)
+//   console.log('next state', store.getState())
+//   console.groupEnd(action.type)
+//   return result
+// }
+// AUTOMATICO
+// import logger from 'redux-logger'
+  
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
 )
+
 
 ReactDOM.render(
   <Provider store={store}>
